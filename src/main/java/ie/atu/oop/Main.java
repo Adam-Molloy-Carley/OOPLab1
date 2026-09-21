312cd8ba5cb96b88489e8a8c16275ed307957b13
AdamMolloyCarley-STU <g00485150@atu.ie>
2026-09-21 13:31:14 +0100

    Lab1 3 books created, firstbook borrowed twice and book one and two details displayed.

--- src/main/java/ie/atu/oop/Main.java
+++ src/main/java/ie/atu/oop/Main.java
@@ -5,11 +5,14 @@ public class Main {
     public static void main(String[] args) {
 
         Book firstBook = createBook("Dune", "Frank-Herbert", 412);
+        Book secondBook = createBook("VerminSlayer", "William-King", 500);
+        Book thirdBook = createBook("DarkBlade", "Dan-abnett", 350);
 
         firstBook.displayDetails();
+        secondBook.displayDetails();
+        thirdBook.displayDetails();
         firstBook.borrowBook();
-
-        Book secondBook = createBook("VerminSlayer", "William-King", 500);
+        firstBook.borrowBook();
 
         secondBook.displayDetails();
         firstBook.displayDetails();
